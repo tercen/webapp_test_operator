f18afb6d8eff67c64c7efba37362f276445694ad
Thiago <tsmonteiro@gmail.com>
2025-02-17 15:26:02 -0300

    update lib dbg

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "606028821"
+    serviceWorkerVersion: "2140863025"
   }
 });
